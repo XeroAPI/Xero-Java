c0d0c12cb280a83d0c66da5df5417e846c587d9b
Jordan Walsh <jordan.walsh@.xero.com>
2017-06-06 13:38:33 +1000

    Added PMD/CPD/Findbugs to the pom.xml 'site' stage.
    Updated the pom file to include static bug checking tools.  This
    produces reports after each 'mvn clean package site' run. 
    
    To skip deployment to the repo you can run it like:
    
    mvn clean package site -DskipDeploy=true
    
    This will generate the reports without any deployment occurring.

--- src/main/java/com/xero/api/Signer.java
+++ src/main/java/com/xero/api/Signer.java
@@ -4,6 +4,6 @@ import com.google.api.client.auth.oauth.OAuthHmacSigner;
 import com.google.api.client.auth.oauth.OAuthRsaSigner;
 
 public interface Signer {
-	public OAuthRsaSigner createRsaSigner();
-	public OAuthHmacSigner createHmacSigner();
+	OAuthRsaSigner createRsaSigner();
+	OAuthHmacSigner createHmacSigner();
 }
---
+++ src/test/java/com/xero/api/tests/XeroClientTests.java
@@ -0,0 +1,39 @@
+package com.xero.api.tests;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.xero.api.XeroClient;
+import com.xero.model.Contact;
+
+public class XeroClientTests {
+	XeroClient client;
+
+	// Squid Proxy Error Code is 407 (Proxy Authentication Required)
+	protected static final int PROXY_ERROR_CODE = 407;
+
+	@Before
+	public void setUp() {
+		client = new XeroClient();
+	}
+
+	@After
+	public void tearDown() {
+		client = null;
+	}
+
+	@Test
+	public void testGetContacts() {
+		List<Contact> contactList;
+		try {
+			contactList = client.getContacts();
+			assert (contactList.size() > 0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
