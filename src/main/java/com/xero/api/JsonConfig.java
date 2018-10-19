4ead85e557dc880b12e1afc75a324ed1c45e7ed3
SidneyAllen <sid.maestre@gmail.com>
2018-10-19 08:25:15 +0100

    Remove config.json for testing
    bump to version 1.3.1

--- src/main/java/com/xero/api/JsonConfig.java
+++ src/main/java/com/xero/api/JsonConfig.java
@@ -131,7 +131,7 @@ public class JsonConfig implements Config {
 
   @Override
   public String getUserAgent() {
-    return USER_AGENT + " " + CONSUMER_KEY + " [Xero-Java-1.3.0]";
+    return USER_AGENT + " " + CONSUMER_KEY + " [Xero-Java-1.3.1]";
   }
 
   @Override
