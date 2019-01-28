a189c27573b77a93ce16d54641d85b6acba8ba5e
SidneyAllen <sid.maestre@gmail.com>
2019-01-28 10:29:30 -0800

    missing semi-colon;

--- src/main/java/com/xero/api/JsonConfig.java
+++ src/main/java/com/xero/api/JsonConfig.java
@@ -15,7 +15,7 @@ import org.json.simple.parser.ParseException;
 
 public class JsonConfig implements Config {
 
-	private String SDK_VERSION = "2.2.14"
+	private String SDK_VERSION = "2.2.14";
   private String APP_TYPE = "Public";
   private String USER_AGENT = "Xero-Java-SDK";
   private String ACCEPT = "application/xml";
