ed0afa0114e463390846782086394416654695ee
SidneyAllen <sid.maestre@gmail.com>
2019-07-31 13:15:41 -0700

    Update AuthenticatedResource.java

--- src/main/java/com/xero/example/AuthenticatedResource.java
+++ src/main/java/com/xero/example/AuthenticatedResource.java
@@ -38,7 +38,7 @@ public class AuthenticatedResource extends HttpServlet
 	final static Logger logger = LogManager.getLogger(AuthenticatedResource.class);
 	final String clientId = "--YOUR_CLIENT_ID--";
 	final String clientSecret = "--YOUR_CLIENT_SECRET--";
-	final String redirectURI = "--YOUR_REDIRECT_URI--";
+	=final String redirectURI = "--YOUR_REDIRECT_URI--";
 	   
 	private String htmlString =  "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">"
 			+ "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">"
