973c1333ab491020723a25b0ae20d3298067934a
SidneyAllen <sid.maestre@gmail.com>
2019-10-07 14:58:02 -0700

    fix security issue with jackson and bump version

--- src/main/java/com/xero/api/client/AccountingApi.java
+++ src/main/java/com/xero/api/client/AccountingApi.java
@@ -99,7 +99,7 @@ public class AccountingApi {
     private ApiClient apiClient;
     private static AccountingApi instance = null;
     private String userAgent = "Default";
-    private String version = "3.0.2";
+    private String version = "3.0.4";
 
     public AccountingApi() {
         this(new ApiClient());
